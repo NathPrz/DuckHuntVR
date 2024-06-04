ffe0823d398993f2eba081d7d16588989d66a0e7
Qiaoqiao <104356305+CocoXXQXX@users.noreply.github.com>
2024-06-04 21:21:06 +0200

    add sound

--- Assets/Fire.cs
+++ Assets/Fire.cs
@@ -9,12 +9,16 @@ public class Fire : MonoBehaviour
     [SerializeField] private Transform spawnPoint;
 
     [SerializeField] private float bulletSpeed = 10f;
+    [SerializeField] private AudioSource audioSource;
+
+
     public void FireBullet()
     {
         GameObject spawnBullet= Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
         spawnBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * bulletSpeed;
         // Adjust the rotation of the bullet by -90 degrees
         spawnBullet.transform.Rotate(-90, 0, 0);
+        audioSource.Play();
         Destroy(spawnBullet, 5f);
     }
 
